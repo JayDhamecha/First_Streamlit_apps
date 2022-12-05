2f03d77a2ce72c6c075984de15e84dd4e57bb103
Jay Dhamecha <118816923+JayDhamecha@users.noreply.github.com>
2022-12-05 17:20:37 +0530

    Update streamlit_apps.py

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -5,3 +5,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Avocado and bread butter')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
