242c4a75b96a1add2c444a644a182cad13f72e3e
Jay Dhamecha <118816923+JayDhamecha@users.noreply.github.com>
2022-12-05 17:12:59 +0530

    Update streamlit_apps.py

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 
-Breakfast Menu
+streamlit.heading('Breakfast Menu')
 streamlit.heading('Bread butter and milk')
-Avocado and milk
-Brown bread and milk
+
